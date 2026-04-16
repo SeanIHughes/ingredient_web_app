6ae454576d81219030e76335e8dba485f6e0db92
Sean Hughes <seanhughes@northai.io>
2026-04-16 16:24:08 -0400

    Final link: Pointing search and scan to 8001

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "137151253" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2868575464" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
